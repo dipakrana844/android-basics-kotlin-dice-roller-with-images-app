ff452b95299957377adca317c93307909410d79f
dipak.rana <dipakrana4608@gmail.com>
2022-07-07 18:42:59 +0530

    Dice app

---
+++ app/src/test/java/com/executor/android_basics_kotlin_dice_roller_with_images_app/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.executor.android_basics_kotlin_dice_roller_with_images_app
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
